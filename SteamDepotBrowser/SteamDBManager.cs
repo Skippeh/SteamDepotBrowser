c477df118495d4912ab61a96cb3ab5d977a53f6c
Skipcast <jonathanjl99@gmail.com>
2020-10-07 23:53:34 +0200

    Fixed caching empty manifest response if response has unexpected html

--- SteamDepotBrowser/SteamDBManager.cs
+++ SteamDepotBrowser/SteamDBManager.cs
@@ -19,9 +19,8 @@ namespace SteamDepotBrowser
                 var result = new List<DepotManifestInfo>();
                 string html = await BaseUrl
                     .AppendPathSegments("depot", depotId, "manifests")
-                    .WithHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36")
-                    .GetStringAsync()
-                    .ConfigureAwait(false);
+                    .WithHeader("User-Agent", "SteamDepotBrowser")
+                    .GetStringAsync().ConfigureAwait(false);
 
                 if (html == null)
                 {
@@ -32,12 +31,12 @@ namespace SteamDepotBrowser
                 var document = await HtmlManager.ParseDocument(html).ConfigureAwait(false);
                 var manifestElements =
                     document.QuerySelector("#manifests")
-                        .QuerySelector("table")
+                        ?.QuerySelector("table")
                         ?.QuerySelector("tbody")
-                        .QuerySelectorAll("tr");
+                        ?.QuerySelectorAll("tr");
 
                 if (manifestElements == null)
-                    return result;
+                    return null;
 
                 foreach (IElement manifestElm in manifestElements)
                 {
